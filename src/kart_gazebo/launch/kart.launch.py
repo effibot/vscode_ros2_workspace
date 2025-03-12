91ad5ebe7cc351d2775b86a7ad381257bff96fc0
effibot <andrea.efficace1@gmail.com>
2025-03-12 17:10:15 +0100

    corrected kart description, working on gazebo sim

---
+++ src/kart_gazebo/launch/kart.launch.py
@@ -0,0 +1,116 @@
+import os
+from launch import LaunchDescription
+import launch
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+import launch_ros
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    pkg_urdf_path = get_package_share_directory('kart_description')
+    pkg_gazebo_path = get_package_share_directory('kart_gazebo')
+
+    gazebo_models_path, ignore_last_dir = os.path.split(pkg_urdf_path)
+    #os.environ["GZ_SIM_RESOURCE_PATH"] += os.pathsep + gazebo_models_path
+
+    rviz_launch_arg = DeclareLaunchArgument(
+        'rviz', default_value='true',
+        description='Open RViz.'
+    )
+
+    world_arg = DeclareLaunchArgument(
+        'world', default_value='empty.sdf',
+        description='Name of the Gazebo world file to load'
+    )
+
+    model_arg = DeclareLaunchArgument(
+        'model', default_value='kart_urdf.urdf',
+        description='Name of the URDF description to load'
+    )
+
+    # Define the path to your URDF or Xacro file
+    urdf_file_path = PathJoinSubstitution([
+        pkg_urdf_path,  # Replace with your package name
+        "urdf",
+        LaunchConfiguration('model')  # Replace with your URDF or Xacro file
+    ])
+
+    world_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_path, 'launch', 'world.launch.py'),
+        ),
+        launch_arguments={
+        'world': LaunchConfiguration('world'),
+        }.items()
+    )
+
+    # Launch rviz
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_urdf_path, 'rviz', 'rviz_conf.rviz')],
+        condition=IfCondition(LaunchConfiguration('rviz')),
+        parameters=[
+            {'use_sim_time': True},
+        ]
+    )
+
+    # Spawn the URDF model using the `/world/<world_name>/create` service
+    spawn_urdf_node = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name", "my_robot",
+            "-topic", "robot_description",
+            "-x", "0.0", "-y", "0.0", "-z", "0.5", "-Y", "0.0"  # Initial spawn position
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': True},
+        ]
+    )
+    #parameters=[
+    #    {'robot_description': launch_ros.descriptions.ParameterValue(
+    #        launch.substitutions.Command(
+    #            ['xacro ',os.path.join(turtlebot2_description_package,'robots/kobuki_conveyor.urdf.xacro')]),
+    #          value_type=str)  }]
+    desc_param = launch_ros.descriptions.ParameterValue(
+        launch.substitutions.Command(
+            ['xacro ', urdf_file_path]), value_type=str
+        )
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': desc_param,
+             'use_sim_time': True},
+        ],
+        remappings=[
+            ('/tf', 'tf'),
+            ('/tf_static', 'tf_static')
+        ]
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+    )
+
+    launchDescriptionObject = LaunchDescription()
+
+    launchDescriptionObject.add_action(rviz_launch_arg)
+    launchDescriptionObject.add_action(world_arg)
+    launchDescriptionObject.add_action(model_arg)
+    launchDescriptionObject.add_action(world_launch)
+    launchDescriptionObject.add_action(rviz_node)
+    launchDescriptionObject.add_action(spawn_urdf_node)
+    launchDescriptionObject.add_action(robot_state_publisher_node)
+    launchDescriptionObject.add_action(joint_state_publisher_gui_node)
+
+    return launchDescriptionObject
